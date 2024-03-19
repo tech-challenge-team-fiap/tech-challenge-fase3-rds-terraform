5a6b6caf23bf40c892e5672e338cb5185c406321
Jair Cavalcante <jaircavalcante@MacBook-Pro-de-Jair.local>
2024-03-18 21:57:02 -0300

    infra terraform and githubaction

---
+++ destroy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Deploy infrastructure using Terraform
+cd infra
+#terraform destroy -auto-approve -var "aws_access_key=${AWS_ACCESS_KEY}" -var "aws_secret_key=${AWS_SECRET_KEY}"
+cd ..
+
+echo "Deployment completed!"
